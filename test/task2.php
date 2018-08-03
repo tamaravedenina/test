c595823eb22cc390cb8d2cf289fb3f709e2215f3
tamaravedenina <36485070+tamaravedenina@users.noreply.github.com>
2018-08-03 23:38:53 +0500

    three

--- test/task2.php
+++ test/task2.php
@@ -1,17 +1,27 @@
 <?php
-function countCells(int $start, int $finish, int $distance) 
+function countCells(int $start, int $finish, int $distance, int &$count) 
 {
-    if ($finish - $start < 2 * $distance) return 1;
-    else return ($finish - $start) % $distance + 1;
+    $mod = ($finish - $start) % $distance;
+    if ($finish - $start >= 2 * $distance) {
+        if ($finish - $start >= 3 * $distance) {
+            for ($i = 0; $i <= $mod; $i++) {
+                countCells($start + $distance + $i, $finish, $distance, $count);
+            }
+        } else $count += $mod + 1;
+    } else $count++;
+    echo $count . "\t";
 }
 
 $postData = file_get_contents('php://input');
 $data = json_decode($postData, true);
 $cells = $data['cells'];
 $distance = $data['distance'];
-$count = 1;
+$countAll = 1;
+echo "\n";
 for ($i = 1; $i < count($cells); $i++) {
-    $count *= countCells($cells[$i-1], $cells[$i], $distance); 
+    $countOne = 0;
+    countCells($cells[$i-1], $cells[$i], $distance, $countOne); 
+    $countAll *= $countOne;
 }
-$resuls = json_encode(['result' => $count]);
+$resuls = json_encode(['result' => $countAll]);
 echo $resuls;
